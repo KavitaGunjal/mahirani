30fc042ba18ca630130aa2542998dca373f35396
kavit <kavit@MSI>
2026-06-09 22:01:51 +0530

    eeeeeeee

---
+++ Newtry2026/src/mytry/Arraylist1.java
@@ -0,0 +1,27 @@
+package mytry;
+
+import java.util.ArrayList;
+
+public class Arraylist1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Stores elements in order
+
+		//Allows duplicates
+
+		//Index-based (like array but grows automatically)
+		
+	        ArrayList<String> names = new ArrayList<>();
+
+	        names.add("Kavita");
+	        names.add("Savita");
+	        names.add("Kavita"); // duplicates allowed
+
+	        System.out.println("List: " + names);
+	        System.out.println("First element: " + names.get(0));
+
+	}
+
+}
